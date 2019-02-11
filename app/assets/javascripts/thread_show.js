790dcec5f4dde3b996182fb2c0b0f0626182885a
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-02-11 18:01:32 +0900

    post

---
+++ app/assets/javascripts/thread_show.js
@@ -0,0 +1,6 @@
+$(function(){
+  $(".thread_post .post_file").on("change",function(){
+    alert("asd");
+    $('.thread_post .img_send').trigger('click');
+  });
+});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,10 @@ class PostsController < ApplicationController
     @groups = Group.all
   end
   def create
-    @post = Post.new(image: params[:post][:image],group_id: params[:post][:group_id],user_id: current_user.id)
-    @post.save
+    if !params[:post][:image].nil? && !params[:post][:group_id].nil?
+      @post = Post.new(image: params[:post][:image],group_id: params[:post][:group_id],user_id: current_user.id)
+      @post.save
+    end
     #respond_to do |format|
         #format.html #{ redirect_to  '/thread/index/'+params[:post][:group_id]}
         #format.json
