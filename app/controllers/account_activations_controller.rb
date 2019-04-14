ce967625cabf21da7b3b81077b26c4ba6d34581a
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-04-14 19:28:34 +0900

    error

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,9 +3,15 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
+
+      p "----------------------1--------------------"
       user.update_attribute(:activated,    true)
+
+      p "----------------------2--------------------"
       user.update_attribute(:activated_at, Time.zone.now)
+      p "----------------------3--------------------"
       log_in user
+      p "----------------------4--------------------"
       flash[:success] = "Account activated!"
       redirect_to root_url
     else
