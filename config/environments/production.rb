c327b2e11043d1ab89bc93e54a63d8ba7716f792
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-04-14 16:23:17 +0900

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default_url_options   = { host: 'glacial-chamber-99021.herokuapp.com' }
   # Do not dump schema after migrations.
   mail = ENV['MAIL_ADDRESS']
   pass = ENV['MAIL_PASSWORD']
