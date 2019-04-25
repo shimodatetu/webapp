c6b8812785bfabab1f3ce85210295fdfa3e02708
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-04-26 06:42:11 +0900

    asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,9 @@ class User < ApplicationRecord
 
   # パスワード再設定のメールを送信する
   def send_password_reset_email
+    p "------------------------------------------"
+    p "------------------------------------------"
+    p "------------------------------------------"
     UserMailer.password_reset(self).deliver_now
   end
   def password_reset_expired?
