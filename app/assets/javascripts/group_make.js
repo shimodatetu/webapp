81d920edf5eed6e8f43c1ee7b5fd77e3858c0dd8
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-05-01 15:09:12 +0900

    asd

--- app/assets/javascripts/group_make.js
+++ app/assets/javascripts/group_make.js
@@ -9,7 +9,24 @@ $(function(){
     //value : en
     $('.make_thread_category .small_select').val(this.id)
     $(".category_modal").modal('hide')
-    $(".now_category").html("<div class='add_category'>" + $(this).attr("value") + "</div><div class='add_category'>＞　" + $(this).attr("name") + "</div>")
+    $(".now_category").html(
+      "<div class='add_category'>"+
+        "<div class='jp'>" +
+          $(this).attr("data-b_name_jp") +
+        "</div>" +
+        "<div class='en'>" +
+          $(this).attr("data-b_name_en") +
+        "</div>" +
+      "</div>"+
+      "　＞"+
+      "<div class='add_category'>"+
+        "<div class='jp'>" +
+          $(this).attr("data-s_name_jp") +
+        "</div>" +
+        "<div class='en'>" +
+          $(this).attr("data-s_name_en") +
+        "</div>" +
+      "</div>")
   })
   $(".scroll_button").on("click",function(){
     $(".category_modal").animate({scrollTop:0},"normal");
