71169e58479fb3d77d300fa819a3da50774d69a1
Tetsunosuke Shimoda <tetuprogram@yahoo.co.jp>
2019-05-09 21:31:00 +0900

    asd

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
     def account_activation(user)
       @user = user
-      mail to: user.email, subject: "Account activation"
+      mail to: user.email, subject: "Account activation / 会員登録の完了"
     end
 
     def password_reset(user)
       @user = user
-      mail to: user.email, subject: "Password reset"
+      mail to: user.email, subject: "Password reset / パスワード変更"
     end
 end
